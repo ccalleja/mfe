34c7d21eb3158a7216f783131ee8ffebce75b236
Clint Calleja <clint.calleja@hotjar.com>
2020-12-23 09:10:58 +0100

    Production config for marketing.

---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,26 @@
+const {merge} = require('webpack-merge');
+const commonConfig = require('./webpack.common');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const {dependencies} = require('../package.json');
+
+// This will be set at build time
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap'
+      },
+      shared: dependencies,
+    }),
+  ],
+}
+
+module.exports = merge(commonConfig, prodConfig);
