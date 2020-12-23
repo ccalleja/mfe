7a81e70f384d9b3e7b6c4d035e1b20975e33aa49
Clint Calleja <clint.calleja@hotjar.com>
2020-12-23 09:05:12 +0100

    Production config for container.

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const {merge} = require('webpack-merge');
+const commonConfig = require('./webpack.common');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const {dependencies} = require('../package.json');
+// This will be set at build time
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].jss'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        'marketing': `marketing@${domain}/marketing/remoteEntry.js`
+      },
+      shared: dependencies,
+    }),
+  ],
+}
+
+module.exports = merge(commonConfig, prodConfig);
