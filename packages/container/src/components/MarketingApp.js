6f2911a9a5176d03d93172d2e60635e779d89857
Clint Calleja <clint.calleja@hotjar.com>
2020-12-23 09:28:40 +0100

    Test change to trigger workflow.

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,6 +1,7 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
 
+// Using the mount and returning a component.
 export default () => {
   const ref = useRef(null);
 
