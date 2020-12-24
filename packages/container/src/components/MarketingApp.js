bd20145750b649b15363966060e981f2ee492a3d
Clint Calleja <clint.calleja@hotjar.com>
2020-12-24 07:40:19 +0100

    Adding navigation between apps.

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,13 +1,24 @@
-import { mount } from 'marketing/MarketingApp';
-import React, { useRef, useEffect } from 'react';
+import {mount} from 'marketing/MarketingApp';
+import React, {useRef, useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
 
 // Using the mount and returning a component.
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+    const {onParentNavigate} = mount(ref.current, {
+      onNavigate: ({pathname: nextPathname}) => {
+        const {pathname} = history.location;
+        pathname !== nextPathname && history.push(nextPathname);
+      }
+    });
 
-  return <div ref={ref} />;
+    history.listen(onParentNavigate);
+    // The empty array means run this effect only once,
+    // first time the app loads
+  }, []);
+
+  return <div ref={ref}/>;
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
+import {createMemoryHistory, createBrowserHistory} from 'history';
 
-const mount = (el) => {
-  ReactDOM.render(<App/>, el);
+const mount = (el, {
+  defaultHistory,
+  onNavigate}) => {
+  const history = defaultHistory || createMemoryHistory();
+  // Listen to changes from the container router history
+  onNavigate && history.listen(onNavigate);
+  ReactDOM.render(<App history={history}/>, el);
+
+  return {
+    onParentNavigate({pathname: nextPathname}) {
+      const {pathname} = history.location;
+      pathname !== nextPathname && history.push(nextPathname);
+    }
+  }
 };
 
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
   if (devRoot) {
-    mount(devRoot)
+    mount(devRoot, {defaultHistory: createBrowserHistory()})
   }
 }
 
