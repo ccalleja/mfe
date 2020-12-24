85944547983596e503ff873122e75e06fc297258
Clint Calleja <clint.calleja@hotjar.com>
2020-12-24 14:40:35 +0100

    Adding the auth app.

---
+++ packages/auth/config/webpack.dev.js
@@ -0,0 +1,34 @@
+const {merge} = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.common');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const {dependencies} = require('../package.json');
+const port = 8082;
+
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    port: port,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+  output:{
+    publicPath: `http://localhost:${port}/`
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'auth',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './AuthApp': './src/bootstrap'
+      },
+      shared: dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App";
+import {createMemoryHistory, createBrowserHistory} from 'history';
+
+const mount = (el, {
+  defaultHistory,
+  onNavigate}) => {
+  const history = defaultHistory || createMemoryHistory();
+  // Listen to changes from the container router history
+  onNavigate && history.listen(onNavigate);
+  ReactDOM.render(<App history={history}/>, el);
+
+  return {
+    onParentNavigate({pathname: nextPathname}) {
+      const {pathname} = history.location;
+      pathname !== nextPathname && history.push(nextPathname);
+    }
+  }
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_auth-dev-root');
+  if (devRoot) {
+    mount(devRoot, {defaultHistory: createBrowserHistory()})
+  }
+}
+
+export {mount};
